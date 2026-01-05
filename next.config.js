acb8ba1e1eebd5b8243e5ed6f279b3c12dad20fb
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-01-05 16:20:47 +0530

    Fixing Auto Update in Next Config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    appDir: true, // ✅ REQUIRED for src/app routes
+  },
+
   images: {
     domains: ['cdn.sanity.io'],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

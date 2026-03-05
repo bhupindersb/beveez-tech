0c18729919c15fc82f52628d6fa2f2b5e75efd0d
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-03-05 13:07:24 +0530

    Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,20 +4,37 @@ export default async function sitemap() {
 
   const blogs = await getBlogs(100)
 
-  const blogUrls = blogs.map((post:any) => ({
+  const blogUrls = blogs.map((post: any) => ({
     url: `https://beveez.tech/blog/${post.slug.current}`,
-    lastModified: post.publishedAt
+    lastModified: post.publishedAt || new Date()
   }))
 
-  return [
+  const staticPages = [
     {
       url: 'https://beveez.tech',
-      lastModified: new Date(),
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/about',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/services',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/start-project',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/free-audit',
+      lastModified: new Date()
     },
     {
       url: 'https://beveez.tech/blog',
-      lastModified: new Date(),
-    },
-    ...blogUrls
+      lastModified: new Date()
+    }
   ]
+
+  return [...staticPages, ...blogUrls]
 }

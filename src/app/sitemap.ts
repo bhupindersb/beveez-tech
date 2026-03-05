e114010c8dac1a68186df28f4e67af05e903fcd6
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-03-05 13:13:40 +0530

    Fix Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -23,17 +23,41 @@ export default async function sitemap() {
       lastModified: new Date()
     },
     {
-      url: 'https://beveez.tech/start-project',
+      url: 'https://beveez.tech/services/website-design-development',
       lastModified: new Date()
     },
     {
-      url: 'https://beveez.tech/free-audit',
+      url: 'https://beveez.tech/services/wordpress-speed-optimization',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/services/headless-wordpress-cms-development',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/services/seo-friendly-website-builds',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/pricing',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/start-your-project',
+      lastModified: new Date()
+    },
+    {
+      url: 'https://beveez.tech/free-website-audit',
       lastModified: new Date()
     },
     {
       url: 'https://beveez.tech/blog',
       lastModified: new Date()
-    }
+    },
+    {
+      url: 'https://beveez.tech/contact',
+      lastModified: new Date()
+    },
   ]
 
   return [...staticPages, ...blogUrls]

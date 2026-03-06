f04b6ef49034ab288fc31ba0b5efe9049c946b5e
Bhupinder Singh <bhupinder@Bhupinders-MacBook-Air.local>
2026-03-06 12:45:23 +0530

    Fix Trust Section

--- src/components/TrustSection.tsx
+++ src/components/TrustSection.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
 
 export default function TrustSection({ data }: any) {
   if (!data || !data.items?.length) return null
@@ -9,9 +10,10 @@ export default function TrustSection({ data }: any) {
 
         {/* Heading with Icon */}
         <div className="flex flex-col max-w-[500px] py-2 mx-auto items-center gap-4 md:flex-row md:justify-center rounded-full bg-[#efd9cb]">
-          {data.headingIcon?.asset?.url && (
+
+          {data.headingIcon && (
             <Image
-              src={data.headingIcon.asset.url}
+              src={urlFor(data.headingIcon).width(96).format('webp').url()}
               alt=""
               width={48}
               height={48}
@@ -22,23 +24,26 @@ export default function TrustSection({ data }: any) {
           <h2 className="font-body text-normal text-darkOrange md:text-normal">
             {data.heading}
           </h2>
+
         </div>
 
         {/* Trust Items */}
         <div className="mt-16 grid grid-cols-2 gap-12 md:grid-cols-4">
+
           {data.items.map((item: any, i: number) => (
             <div
               key={i}
               className="flex flex-col items-center gap-4 text-center"
             >
-              {item.icon?.asset?.url && (
+
+              {item.icon && (
                 <div className="trust-icon">
                   <Image
-                    src={item.icon.asset.url}
-                    alt={item.title}
+                    src={urlFor(item.icon).width(280).format('webp').url()}
+                    alt={item.title || ''}
                     width={140}
                     height={140}
-                    className='mix-blend-multiply'
+                    className="mix-blend-multiply"
                   />
                 </div>
               )}
@@ -46,8 +51,10 @@ export default function TrustSection({ data }: any) {
               <p className="text-[22px] font-heading font-demi text-darkBlue">
                 {item.title}
               </p>
+
             </div>
           ))}
+
         </div>
 
       </div>
